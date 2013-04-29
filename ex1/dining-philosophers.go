99fab9186d2de9300d5ea44d06472825893d420a
Patrick Andrew <missionsix@gmail.com>
2013-04-29 14:58:15 -0700

    Update dining-philosophers.go
    Update header and replace 8 space tabs with 4.

--- ex1/dining-philosophers.go
+++ ex1/dining-philosophers.go
@@ -1,6 +1,11 @@
-/*
+/* Editor Settings: expandtabs and use 4 spaces for indentation                                                                                                                                            
+ * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *                                                                                                                                               
+ * -*- mode: c, c-basic-offset: 4 -*- */
 
-*/
+/* Copyright (C) Patrick Andrew. All Rights Reserved
+ *
+ * Authors: Patrick Andrew <missionsix@gmail.com>
+ */
 package main
 
 import (
