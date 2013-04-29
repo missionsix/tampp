038c18c1e4320ac41ca30d7d63323ff1eb4208e1
Patrick Andrew <pandrew@isilon.com>
2013-04-29 15:00:35 -0700

    Fix extraneous whitespace from editor settings copy & paste

--- ex1/dining-philosophers.go
+++ ex1/dining-philosophers.go
@@ -1,5 +1,5 @@
-/* Editor Settings: expandtabs and use 4 spaces for indentation                                                                                                                                            
- * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *                                                                                                                                               
+/* Editor Settings: expandtabs and use 4 spaces for indentation
+ * ex: set softtabstop=4 tabstop=8 expandtab shiftwidth=4: *
  * -*- mode: c, c-basic-offset: 4 -*- */
 
 /* Copyright (C) Patrick Andrew. All Rights Reserved
