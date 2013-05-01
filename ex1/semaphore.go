148108279917109025b3d06cc911df96b72b5910
Patrick Andrew <pandrew@isilon.com>
2013-04-30 17:31:29 -0700

    Updating documentation on Semaphore package.

--- ex1/semaphore.go
+++ ex1/semaphore.go
@@ -6,11 +6,19 @@
  * Authors: Patrick Andrew <missionsix@gmail.com>
  */
 
+// Package Semaphore provides a high level construct of semaphores
+// implemented using Go channels.
 package Semaphore
 
+// Empty is an empty struct acting as a single resource
 type empty struct {}
+
+// Semaphore is the actual type. Declare with := make(Semaphore, N)
+// where N is the number of resources available.
 type Semaphore chan empty
 
+// P is the wait construct
+// change n to consume n resources
 func (s Sempahore) P(n int) {
 	e := empty{}
 	for i := 0; i < n; i++ {
@@ -18,6 +26,8 @@ func (s Sempahore) P(n int) {
 	}
 }
 
+// V is the signal construct of the Semaphore
+// signal n resources at a time
 func (s Sempahore) V(n int) {
 	for i := 0; i < n; i++ {
 		<- s
